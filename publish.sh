0f074a7426e20e9eef7bf770d6041e59b7478808
abdellatif Temsamani <contact@abdellatif.dev>
2025-05-08 23:42:05 +0000

    Fix

--- publish.sh
+++ publish.sh
@@ -9,7 +9,7 @@ pnpm publish
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 if [ "$BRANCH" = "dev" ]; then
-    pnpm publish --access public --tag rc
+    pnpm publish --access public --tag=rc
 else
     pnpm publish --access public
 fi
